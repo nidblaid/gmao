82660dff2481ac173b78f8cbcdc855e7ff7be0a2
nidblaid <110823482+nidblaid@users.noreply.github.com>
2023-08-31 22:12:22 +0100

    Update mrp_production.py

--- gmao/models/mrp_production.py
+++ gmao/models/mrp_production.py
@@ -27,19 +27,5 @@ class FleetVehicle(models.Model):
 
     expenses_count = fields.Integer()
 
-    def get_expenses(self):
-        self.ensure_one()
-        return {
-            'type': 'ir.actions.act_window',
-            'name': 'Dépenses',
-            'view_mode': 'tree',
-            'res_model': 'stock.picking',
-            'domain': [('vehicle_id', '=', self.id)],
-            'context': "{'create': False}"
-        }
-
-    def expenses_count(self):
-        for record in self:
-            record.expenses_count = self.env['stock.picking'].search_count(
-                [('vehicle_id', '=', self.id)])
+    
     
