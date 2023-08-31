2f41301bcdd6ca16c18a62f54e9137e81b7dc71b
nidblaid <110823482+nidblaid@users.noreply.github.com>
2023-08-31 21:38:48 +0100

    Update mrp_production.py

--- gmao/models/mrp_production.py
+++ gmao/models/mrp_production.py
@@ -11,7 +11,35 @@ class FleetVehicleLogServices(models.Model):
 
     gmao_id = fields.Many2one('gmao.model', string="Gmao ID")
 
-class MaintenanceRequest(models.Model):
-    _inherit = 'maintenance.request'
+class RepairOrder(models.Model):
+    _inherit = 'repair.order'
 
     gmao_id = fields.Many2one('gmao.model', string="Gmao ID")
+
+class StockPicking(models.Model):
+    _inherit = 'stock.picking'
+
+    vehicle_id = fields.Many2one('fleet.vehicle', string="Véhicule")
+    equipment_id = fields.Many2one('maintenance.equipment', string="Equipement")
+
+class FleetVehicle(models.Model):
+    _inherit = 'fleet.vehicle'
+
+    expenses_count = fields.Integer(string="Dépenses", _compute='expenses_count')
+
+    def get_expenses(self):
+        self.ensure_one()
+        return {
+            'type': 'ir.actions.act_window',
+            'name': 'Dépenses',
+            'view_mode': 'tree',
+            'res_model': 'stock.picking',
+            'domain': [('vehicle_id', '=', self.id)],
+            'context': "{'create': False}"
+        }
+
+    def expenses_count(self):
+        for record in self:
+            record.expenses_count = self.env['stock.picking'].search_count(
+                [('vehicle_id', '=', self.id)])
+    
