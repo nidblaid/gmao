88aa79a6543057dacce3a16603d94703ae141ab2
nidblaid <110823482+nidblaid@users.noreply.github.com>
2023-09-01 16:26:28 +0100

    Update mrp_production.py

--- gmao/models/mrp_production.py
+++ gmao/models/mrp_production.py
@@ -26,24 +26,25 @@ class StockPicking(models.Model):
 class FleetVehicle(models.Model):
     _inherit = 'fleet.vehicle'
 
-    expenses_vehicle_count = fields.Integer(compute='_expenses_count')
+    pdr_outgoing = fields.Integer(compute='_pdr_outgoing_count')
     deliveries_vehicle_count = fields.Integer(compute='_deliveries_count')
+    pdr_incoming = fields.Integer(compute='_deliveries_count')
     
-    def get_expenses(self):
+    def get_pdr_outgoing(self):
         self.ensure_one()
         return {
             'type': 'ir.actions.act_window',
             'name': 'Dépenses',
             'view_mode': 'tree',
             'res_model': 'stock.picking',
-            'domain': [('vehicle_id', '=', self.id),('is_pdr', '=', True)],
+            'domain': [('vehicle_id', '=', self.id),('is_pdr', '=', True),('state', '=', 'done'),('picking_type_id.code', '=', 'outgoing')],
             'context': "{'create': False}"
         }
 
-    def _expenses_count(self):
+    def _pdr_outgoing_count(self):
         for record in self:
-            record.expenses_vehicle_count = self.env['stock.picking'].search_count(
-                [('vehicle_id', '=', self.id),('is_pdr', '=', True)])
+            record.pdr_outgoing = self.env['stock.picking'].search_count(
+                [('vehicle_id', '=', self.id),('is_pdr', '=', True),('state', '=', 'done'),('picking_type_id.code', '=', 'outgoing')])
 
     def get_deliveries(self):
         self.ensure_one()
@@ -59,7 +60,7 @@ class FleetVehicle(models.Model):
     def _deliveries_count(self):
         for record in self:
             record.deliveries_vehicle_count = self.env['stock.picking'].search_count(
-                [('vehicle_id', '=', self.id),('is_pdr', '=', False)])
+                [('vehicle_id', '=', self.id),('is_pdr', '=', False),('state', '=', 'done'),('picking_type_id.code', '=', 'outgoing')])
 
     
     
