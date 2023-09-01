6dc39dd3c4cadd6dacaf1186fd4132acd5548530
nidblaid <110823482+nidblaid@users.noreply.github.com>
2023-09-01 22:38:37 +0100

    Update update_results.py

--- gmao/wizard/update_results.py
+++ gmao/wizard/update_results.py
@@ -36,16 +36,8 @@ class UpdateResults(models.TransientModel):
 
         for vehicle in vehicles:
             vehicle_record = gmao_analytics.search([])
-            deliveries_domain = [('vehicle_id','=', vehicle.id),('state', '=', 'done'),('picking_type_id.code', '=', 'outgoing')]
-            repairs_domain  = [('vehicle_id','=', vehicle.id),('state','=', 'done')]
-            
-            if self.start_date:
-                deliveries_domain.append(('create_date', '>=', self.start_date))
-                repairs_domain.append(('create_date', '>=', self.start_date))
-                
-            if self.end_date:
-                deliveries_domain.append(('create_date', '>=', self.end_date))
-                repairs_domain.append(('create_date', '>=', self.end_date))
+            deliveries_domain = [('vehicle_id','=', vehicle.id),('state', '=', 'done'),('picking_type_id.code', '=', 'outgoing'),('create_date', '>=', self.start_date),('create_date', '>=', self.start_date)]
+            repairs_domain  = [('vehicle_id','=', vehicle.id),('state','=', 'done'),('create_date', '>=', self.start_date),('create_date', '>=', self.start_date)]
                 
             total_deliveries = self.env['stock.picking'].search(deliveries_domain).mapped('somme')
             total_pdr_outgoing = self.env['repair.order'].search(repairs_domain).mapped('amount_total')
